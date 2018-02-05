581bf46631a17a4fd3fd0a1911473a84140b1de8
TylerOrcutt <TylerOrcutt@users.noreply.github.com>
2018-02-05 13:05:01 -0500

    merger

---
+++ ExcelFileMerger/ExcelFileMerger/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ExcelFileMerger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusText1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(420, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(302, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(420, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Merge";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input FIle";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output File:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(83, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(302, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusText1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 129);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(533, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusText1
+            // 
+            this.statusText1.Name = "statusText1";
+            this.statusText1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 151);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Excel Tab Merger";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusText1;
+    }
+}
+
---
+++ ExcelFileMerger/ExcelFileMerger/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+namespace ExcelFileMerger
+{
+    public partial class Form1 : Form
+    {
+        string dirpath = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "Excel Workbook (*.xlsx) | *.xlsx";
+            DialogResult res = fd.ShowDialog();
+            if (res != DialogResult.OK)
+            {
+                return;
+            }
+            textBox1.Text = fd.FileName;
+            string file = fd.FileName;
+
+            string[] fpath = file.Split('\\');
+            string outfileName = fpath[fpath.Length - 1].Substring(0, fpath[fpath.Length - 1].Length - 5) + "_OUTPUT.csv";
+            string savepath = "";
+            for (int i = 0; i < fpath.Length - 1; i++)
+            {
+                savepath += fpath[i] + "\\";
+            }
+            textBox2.Text = savepath + outfileName;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { 
+
+            Thread t = new Thread(() =>
+            {
+                string file = textBox1.Text;
+                OpenFileDialog fd = new OpenFileDialog();
+                fd.FileName = file;
+                if (!fd.CheckFileExists)
+                {
+                    MessageBox.Show("Invalid file");
+                    return;
+                }
+                string[] fpath = file.Split('\\');
+                Console.WriteLine("File Name =" + fpath[fpath.Length - 1].Substring(0, fpath[fpath.Length - 1].Length - 5));
+                string outfileName = fpath[fpath.Length - 1].Substring(0, fpath[fpath.Length - 1].Length - 5) + "_OUTPUT.csv";
+                string savepath = "";
+                for (int i = 0; i < fpath.Length - 1; i++)
+                {
+                    savepath += fpath[i] + "\\";
+                }
+
+
+                Console.WriteLine("Reading file");
+                updateStatusText("Opening workbook...");
+                string constring = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + file + ";Extended Properties=\"Excel 8.0;HDR=YES;IMEX=1;\"";
+                OleDbConnection con = new OleDbConnection(constring);
+                try
+                {
+                    con.Open();
+                }
+                catch (Exception)
+                {
+                 
+                    MessageBox.Show("Error opening " + file + ". Is the file already open?");
+                    return;
+                }
+                DataTable dtSheet = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                List<string> sheelist = new List<string>();
+                foreach (DataRow drSheet in dtSheet.Rows)
+                {
+                    if (drSheet["TABLE_NAME"].ToString().Contains("$"))
+                    {
+                        sheelist.Add(drSheet["TABLE_NAME"].ToString());
+                        Console.WriteLine(drSheet["TABLE_NAME"].ToString());
+                    }
+                }
+                OleDbCommand cmd;
+                string command = "select * from [" + sheelist.ElementAt(0) + "]";
+                updateStatusText("Selecting data....");
+                for (int i = 1; i < dtSheet.Rows.Count; i++)
+                {
+
+
+                    command += " union select * from [" + sheelist.ElementAt(i) + "]";
+
+                
+                }
+                Console.WriteLine(command);
+                Console.WriteLine("Creating data set");
+                updateStatusText("Creating new dataset...");
+                cmd = new OleDbCommand(command, con);
+                OleDbDataAdapter adpt = new OleDbDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adpt.Fill(dt);
+                Console.WriteLine("creating workbook");
+                updateStatusText("Writing output file...");
+                /* dataTable to csv --from stack*/
+                StringBuilder sb = new StringBuilder();
+
+                IEnumerable<string> columnNames = dt.Columns.Cast<DataColumn>().
+                                                  Select(column => column.ColumnName);
+                sb.AppendLine(string.Join(",", columnNames));
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    IEnumerable<string> fields = row.ItemArray.Select(field => ("\"" + field.ToString() + "\""));
+                    sb.AppendLine(string.Join(",", fields));
+                }
+
+                File.WriteAllText(savepath + outfileName, sb.ToString());
+
+                updateStatusText("Done");
+                dt.Clear();
+                con.Close();
+            });
+            t.Start();
+        }
+        void updateStatusText(string text)
+        {
+            try
+            {
+                this.Invoke((MethodInvoker)(() =>
+                {
+                    this.statusText1.Text = text;
+                }));
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
